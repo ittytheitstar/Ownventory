24fb137170b90bbfe2e913bd4be74c2ee6858763
= <=>
2026-03-16 21:27:45 +1300

    anoth prisma version vix

--- frontend/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'prisma';
-
-export default defineConfig({
-  // Migrate and other Prisma tooling will read the datasource URL from here.
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-});
